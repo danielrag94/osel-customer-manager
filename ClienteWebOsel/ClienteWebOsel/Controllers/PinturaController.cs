098d64ebab938d499e035b1c3e405745b6785f32
danielrag94 <danielrag94@gmail.com>
2017-11-19 13:44:37 -0700

    Cliente web de pruebas creado
    Creación de un cliente web para probar los servicios.

---
+++ ClienteWebOsel/ClienteWebOsel/Controllers/PinturaController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClienteWebOsel.Models;
+using ClienteWebOsel.ViewModels;
+
+namespace ClienteWebOsel.Controllers
+{
+    public class PinturaController : Controller
+    {
+        // GET: Pintura
+        public ActionResult Index()
+        {
+            ClienteServicioPintura csp = new ClienteServicioPintura();
+            ViewBag.listaPinturas = csp.LeerTodos();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Crear()
+        {            
+            return View("Crear");
+        }
+
+        [HttpPost]
+        public ActionResult Crear(ViewModelsPintura vmp)
+        {
+            ClienteServicioPintura csp = new ClienteServicioPintura();
+            csp.Crear(vmp.Pintura);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Eliminar(string id)
+        {
+            ClienteServicioPintura csp = new ClienteServicioPintura();
+            csp.Eliminar(csp.LeerPorId(id));
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Editar(string id)
+        {
+            ClienteServicioPintura csp = new ClienteServicioPintura();
+            ViewModelsPintura vmp = new ViewModelsPintura();
+            vmp.Pintura = csp.LeerPorId(id);
+            return View("Editar", vmp);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ViewModelsPintura vmp)
+        {
+            ClienteServicioPintura csp = new ClienteServicioPintura();
+            csp.Editar(vmp.Pintura);            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClienteWebOsel/ClienteWebOsel/Models/Pintura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClienteWebOsel.Models
+{
+    public class Pintura
+    {
+        [Display(Name = "Id")]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Nombre")]
+        public string Nombre
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Codigo")]
+        public string Codigo
+        {
+            get;
+            set;
+        }
+    }
+}
