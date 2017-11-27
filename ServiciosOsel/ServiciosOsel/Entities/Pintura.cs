6724883cf0d46ea6f0ae10264c55f12ac5e8f52c
danielrag94 <danielrag94@gmail.com>
2017-11-27 16:50:51 -0700

    Servicios web de la aplicación
    Modificados los servicios para actualizar el campo Color.

--- ServiciosOsel/ServiciosOsel/Entities/Pintura.cs
+++ ServiciosOsel/ServiciosOsel/Entities/Pintura.cs
@@ -17,5 +17,6 @@ namespace ServiciosOsel.Entities
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Codigo { get; set; }
+        public string Color { get; set; }
     }
 }
