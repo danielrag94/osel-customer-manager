aafdf1a4d077a1b48321fc5e56e84bc1ecaca48f
danielrag94 <danielrag94@gmail.com>
2017-12-01 11:05:14 -0700

    Cliente web de la aplicación
    Se añadieron vistas de cliente.

--- ClienteWebOsel/ClienteWebOsel/App_Start/RouteConfig.cs
+++ ClienteWebOsel/ClienteWebOsel/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ClienteWebOsel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Pintura", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
